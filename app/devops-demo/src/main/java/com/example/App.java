17b1103deffd443514dd64ef77797c1e26bdac95
Mihaela-Loredana Ignat <mihaela-loredana.ignat@student.tuiasi.ro>
2026-01-20 15:18:07 +0200

    Small change to trigger CI/CD pipeline

--- app/devops-demo/src/main/java/com/example/App.java
+++ app/devops-demo/src/main/java/com/example/App.java
@@ -9,5 +9,7 @@ public class App
 		port(3000); // schimbă portul la 3000
 		get("/", (req,res) -> "Hello devops!");
         System.out.println( "App is running on port 3000, but 4567 - implicit for Spark Java-micro-framework for API");
+        System.out.println( " Small change");
+
     }
 }
